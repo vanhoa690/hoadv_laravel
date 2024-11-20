f357f30cb2e92217f136034a119b0fdc54e5a388
vanhoa690 <vanhoa690@gmail.com>
2024-11-20 14:24:27 +0700

    add route register and login

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Log;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 
@@ -24,7 +23,32 @@ class AuthController extends Controller
             User::create($payload);
             return response()->json(["status" => 200, "message" => "Account created successfully!"]);
         } catch (\Exception $err) {
-            // Log::info("user_register_err =>" . $err->getMessage());
+            return response()->json(["status" => 500, "message" => "Something went wrong!"], 500);
+        }
+    }
+
+    // * Login user
+    public function login(Request $request)
+    {
+        $payload = $request->validate([
+            "email" => "required|email",
+            "password" => "required"
+        ]);
+
+        try {
+            $user = User::where("email", $payload["email"])->first();
+            if ($user) {
+                // * Check password
+                if (!Hash::check($payload["password"], $user->password)) {
+                    return response()->json(["status" => 401, "message" => "Invalid credentials."]);
+                }
+
+                $token = $user->createToken("web")->plainTextToken;
+                $authRes = array_merge($user->toArray(), ["token" => $token]);
+                return ["status" => 200, "user" => $authRes, "message" => "Loggedin succssfully!"];
+            }
+            return response()->json(["status" => 401, "message" => "No account found with these credentials."]);
+        } catch (\Exception $err) {
             return response()->json(["status" => 500, "message" => "Something went wrong!"], 500);
         }
     }
--- routes/api.php
+++ routes/api.php
@@ -3,5 +3,5 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// Route::post("/auth/login", [AuthController::class, 'login']);
+Route::post("/auth/login", [AuthController::class, 'login']);
 Route::post("/auth/register", [AuthController::class, 'register']);
