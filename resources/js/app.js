1933c54ee2bc093a6db96f7345951daaafc51e7a
vanhoa690 <vanhoa690@gmail.com>
2024-11-22 10:43:07 +0700

    tich hop vuejs laravel 11

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,5 @@
-import './bootstrap';
+import "./bootstrap";
+import { createApp } from "vue";
+import App from "./src/App.vue";
+
+createApp(App).mount("#app");
---
+++ resources/js/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <h2>App Vue</h2>
+    <RouterView />
+</template>
+
+<style scoped></style>
