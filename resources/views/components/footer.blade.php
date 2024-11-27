9bdfe8228d27326f77ceaa77aba2e52480d75a8a
vanhoa690 <vanhoa690@gmail.com>
2024-11-27 22:45:46 +0700

    add layout header footer

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="bg-violet-600">
+    <div class="flex flex-col gap-8 container py-10 px-4">
+        <div class="flex items-center gap-8 justify-between">
+        </div>
+        <div class="text-center">
+            <span class="text-sm text-violet-200">
+                &copy; {{ config('app.name') }} {{ date('Y') }}. All rights reserved.
+            </span>
+        </div>
+    </div>
+</footer>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,3 +1,29 @@
-<x-app-layout>
-    <h1>Homepage</h1>
-</x-app-layout>
+<x-layout>
+    <section class="w-full">
+        <div class="container flex flex-col gap-8 pb-24 px-4">
+            <h2 class="text-center font-bold text-4xl">Latest Courses</h2>
+            <div class="grid grid-cols-2 gap-4">
+                @foreach ($courses as $course)
+                <a href="/" class="rounded-md bg-gray-100 hover:bg-gray-200">
+                    <article class="flex flex-col p-4 h-full">
+                        <header>
+                            <h3 class="font-semibold text-xl">Title</h3>
+                        </header>
+                        <p class="font-normal mb-8 mt-2">description</p>
+                        <footer class="flex gap-2 justify-between mt-auto">
+                            <div class="flex items-center gap-2">
+                                <x-icon name="film" class="size-4" />
+                                <span class="text-sm font-semibold">11 lessons</span>
+                            </div>
+                            <div class="flex items-center gap-2">
+                                <x-icon name="clock" class="size-4" />
+                                <span class="text-sm font-semibold">2</span>
+                            </div>
+                        </footer>
+                    </article>
+                </a>
+                @endforeach
+            </div>
+        </div>
+    </section>
+</x-layout>
