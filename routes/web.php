8f351049c63882c8724311772cec10cca7483e5e
vanhoa690 <vanhoa690@gmail.com>
2024-12-09 09:27:40 +0700

    add template homepage

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('homepage');
+});
+
+Route::get('/', function () {
+    return view('homepage');
 });
